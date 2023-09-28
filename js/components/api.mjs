f36d68d5548f3fd2d8207917b5ec78367cfee409
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-09-28 21:02:02 +0200

    Added API endpoints

---
+++ js/components/api.mjs
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "https://api.noroff.dev";
+export const registerURL = `${API_BASE_URL}/api/v1/social/auth/register/`;
+export const loginURL = `${API_BASE_URL}/api/v1/social/auth/login/`;
