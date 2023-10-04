d08999ec37fc4dc9537666770ff78f02adb0a15b
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-10-04 14:48:10 +0200

    Created function to check if a user is logged in

--- scripts/pages/login.mjs
+++ scripts/pages/login.mjs
@@ -1,4 +1,14 @@
 import { onLoginFormSubmit } from "../utils/onLoginFormSubmit.mjs";
+import { checkIfLoggedIn } from "../utils/checkIfLoggedIn.js";
 
-const loginForm = document.querySelector("#login-form");
-loginForm.addEventListener("submit", onLoginFormSubmit);
+function login() {
+  try {
+    const loginForm = document.querySelector("#login-form");
+    loginForm.addEventListener("submit", onLoginFormSubmit);
+
+    checkIfLoggedIn();
+  } catch (error) {
+    console.log(error);
+  }
+}
+login();
