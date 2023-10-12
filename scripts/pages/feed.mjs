02f5acb4161a2e641cbeb5966852dcff19c76f38
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-10-12 02:42:11 +0200

    Function to filter posts and added clearHTML

--- scripts/pages/feed.mjs
+++ scripts/pages/feed.mjs
@@ -2,15 +2,20 @@ import { renderPosts } from "../components/render.mjs";
 import { getPosts } from "../utils/getPosts.mjs";
 import { onCreatePostFormSubmit } from "../utils/onCreatePostFormSubmit.mjs";
 import { createMessage } from "../components/createMessage.js";
+import { clearHTML } from "../components/clearHTML.js";
+import { onFilterFormSubmit } from "../utils/onFilterFormSubmit.mjs";
 
 const feedPostsContainer = document.querySelector("#feed-container");
+
+const filterSelect = document.querySelector("#filterPostsForm");
+filterSelect.addEventListener("submit", onFilterFormSubmit);
 
 async function feed() {
   try {
     const posts = await getPosts();
     const form = document.querySelector("#form-new-post");
     form.addEventListener("submit", onCreatePostFormSubmit);
-
+    clearHTML(feedPostsContainer);
     renderPosts(posts, feedPostsContainer);
   } catch (error) {
     console.log(error);
