adcf48540a765ba68cd79edadf517bb179a640bd
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-10-13 13:34:36 +0200

    Included the search to filter by username

--- scripts/utils/filterPostsOnSearch.js
+++ scripts/utils/filterPostsOnSearch.js
@@ -1,5 +1,5 @@
 export function filterPostsOnSearch(posts, term) {
   const searchTerm = term.toLowerCase().trim();
-  const filteredPosts = posts.filter(({ title, body }) => title.toLowerCase().trim().includes(searchTerm) || body.toLowerCase().trim().includes(searchTerm));
+  const filteredPosts = posts.filter(({ title, body, author }) => title.toLowerCase().trim().includes(searchTerm) || body.toLowerCase().trim().includes(searchTerm) || author.name.toLowerCase().trim().includes(searchTerm));
   return filteredPosts;
 }
